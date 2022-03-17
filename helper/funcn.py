90d518092f66ce7b5397bf0981cc529beb65107a
adamammofake <86252588+adamammofake@users.noreply.github.com>
2022-03-17 05:46:14 +0530

    Update funcn.py

--- helper/funcn.py
+++ helper/funcn.py
@@ -18,7 +18,7 @@ COUNT = []
 OK = {}
 
 uptime = dt.now()
-os.system("wget https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg -O thumb.jpg")
+os.system("wget https://telegra.ph/file/2e33c6bcb2a139e631260.jpg -O thumb.jpg")
 
 if not os.path.isdir("downloads/"):
     os.mkdir("downloads/")
